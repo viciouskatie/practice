320db7ee400c834fc2e239d962fb30461af55acc
viciouskatie <53979408+viciouskatie@users.noreply.github.com>
2019-09-26 20:19:24 -0500

    added data

---
+++ practice.js
@@ -0,0 +1,21 @@
+function animal(id, name, age, breed){
+  this.id = id,
+  this.name = name,
+  this.age = age,
+  this.breed = breed;
+
+}
+
+let giz = new animal(1, "Gizmo", 3, "Chihuahua" )
+let lil = new animal(2, "Lily", 3, "Pitbull" )
+let rox = new animal(3, "Roxy", 12, "Pitbull" )
+let diesel = new animal(4, "Diesel", 1, "American Bully")
+
+let pets = [giz, lil, rox, diesel]
+
+// using map
+pets.map(animal => {
+  Object.keys(animal).map(key => {
+    console.log(key, animal[key])
+  })
+})
